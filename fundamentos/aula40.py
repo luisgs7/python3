44cda279dd9399b370c3a52e4f92646b9e1d896a
luisgs7 <luisgsilva270@gmail.com>
2021-07-19 12:26:56 -0300

    aula40

---
+++ fundamentos/aula40.py
@@ -0,0 +1,14 @@
+type(1)
+__builtins__.type('Ola')
+__builtins__.print(10 / 3)
+
+__builtins__.help(__builtins__.dir)
+
+nome = "Pedro"
+type(nome)
+
+__builtins__.len(nome)
+
+# dir()
+
+__builtins__
