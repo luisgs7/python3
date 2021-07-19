776dd37c12171a54a97a87039e1618dc8c5ddf1d
luisgs7 <luisgsilva270@gmail.com>
2021-07-19 12:25:00 -0300

    aula33

---
+++ fundamentos/aula33.py
@@ -0,0 +1,24 @@
+salario = 3450.45
+despesas = 2456.2
+
+percentual_comprometido = despesas / salario * 100
+
+print(percentual_comprometido)
+
+a = 1
+
+a += 5 
+
+a -= 3
+
+a *= 2
+
+a /= 2
+
+a %= 4
+
+a **= 8
+
+a //= 256
+
+print(a)
