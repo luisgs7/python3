cc4ec30c02924f1e219299c112063a7170791481
luisgs7 <luisgsilva270@gmail.com>
2021-07-19 12:28:21 -0300

    aula44

---
+++ fundamentos/aula44.py
@@ -0,0 +1,15 @@
+import decimal
+from decimal import Decimal, getcontext
+
+print(Decimal(1) / Decimal(7))
+
+getcontext().prec = 4
+
+print(Decimal(1) / Decimal(7))
+
+print(Decimal.max(Decimal(1), Decimal(7)))
+
+print(1.1 + 2.2)
+print(Decimal(1.1) + Decimal(2.2))
+
+# print(dir(decimal))
