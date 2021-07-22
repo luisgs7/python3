87225275d95c2842b79baec69c9f195debf2c6a6
luisgs7 <luisgsilva270@gmail.com>
2021-07-22 14:19:31 -0300

    aula46.py

---
+++ fundamentos/aula46.py
@@ -0,0 +1,19 @@
+nome = 'João Pedro'
+
+print(nome[0])
+print(nome[6])
+print(nome[-3])
+print(nome[4:])
+print(nome[-5:])
+print(nome[:3])
+print(nome[2:5])
+
+numeros = '1234567890'
+
+print(numeros[::])
+print(numeros[::2])
+print(numeros[1::2])
+print(numeros[::-1])
+print(numeros[::-2])
+
+print(nome[::-1])
