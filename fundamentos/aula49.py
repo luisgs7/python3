a691cd494e10300221384ec697ecf99d824e7a79
luisgs7 <luisgsilva270@gmail.com>
2021-07-22 14:20:02 -0300

    aula49.py

---
+++ fundamentos/aula49.py
@@ -0,0 +1,21 @@
+lista = []
+
+print(type(lista))
+print(dir(lista))
+
+print(len(lista))
+
+lista.append(1)
+lista.append(7)
+
+print(lista)
+print(len(lista))
+
+new_lista = [1, 2, 'linux', 'fedora']
+print(new_lista)
+
+new_lista.remove(2)
+print(new_lista)
+
+new_lista.reverse()
+print(new_lista)
