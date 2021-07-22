1ae3ab42462978a2092e646541fd069ad89e01b9
luisgs7 <luisgsilva270@gmail.com>
2021-07-22 14:19:47 -0300

    aula47.py

---
+++ fundamentos/aula47.py
@@ -0,0 +1,12 @@
+frase = "Python é uma linguagem excelente"
+
+print('py' not in frase)
+print('ing' in frase)
+
+print(len(frase))
+
+print(frase.lower())
+print(frase.upper())
+
+print(frase.split())
+print(frase.split('a'))
