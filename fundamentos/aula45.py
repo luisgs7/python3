19cb4313b3f44cfaa73fafb707bdc79aa8de2aed
luisgs7 <luisgsilva270@gmail.com>
2021-07-22 14:19:15 -0300

    aula45.py

---
+++ fundamentos/aula45.py
@@ -0,0 +1,11 @@
+nome = "São Pedro"
+print(nome[0])
+
+texto = 'Texto entre apostrófos pode ter "aspas"'
+print(texto)
+
+text = "Dias D'avila"  # 'Dias D\'avila'
+print(text)
+
+doc = """Texto com múltipas\n\t...linhas"""
+print(doc)
