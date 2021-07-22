99aad285a3f9322ac3e63a650c06ed6fc1ed5826
luisgs7 <luisgsilva270@gmail.com>
2021-07-22 14:20:53 -0300

    aula51.py

---
+++ fundamentos/aula51.py
@@ -0,0 +1,15 @@
+lista = ['mint', 'fedora', 'ubuntu', 'debian', 'kde']
+
+print(lista[1:3])
+print(lista[1:-1])
+print(lista[1:])
+print(lista[:-1])
+print(lista[:])
+print(lista[::2])
+print(lista[::-1])
+
+del lista[2]
+print(lista)
+
+del lista[1:]
+print(lista)
