fe24022d5c9a69db9d2f6fb0ef3b211bdeb8fb97
luisgs7 <luisgsilva270@gmail.com>
2021-07-22 14:20:17 -0300

    aula50.py

---
+++ fundamentos/aula50.py
@@ -0,0 +1,13 @@
+lista = [1, 5, 'linux', 'ubuntu', 3.1415]
+
+print(lista.index('linux'))
+
+print(lista[2])
+
+print(1 in lista)
+
+print('fedora' not in lista)
+
+print(lista[0])
+print(lista[-1])
+print(lista[-5])
