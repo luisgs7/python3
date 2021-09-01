3b820628a932bd5bb04138ac85776d4b21d8c862
luisgs7 <luisgsilva270@gmail.com>
2021-09-01 16:03:24 -0300

    io_v4

---
+++ manipulacao_arquivos/io_v4.py
@@ -0,0 +1,14 @@
+#! /usr/bin/python
+try:
+    arquivo = open('pessoas.csv')
+
+    for registro in arquivo:
+        print('Nome: {}, Idade: {}'.format(*registro.strip().split(',')))
+
+
+finally:
+    print('finally')
+    arquivo.close()
+
+if arquivo.closed:
+    print('Arquivo já foi fechado!')
