04c88635367796649f76ab5e1d8471a21e03b122
luisgs7 <luisgsilva270@gmail.com>
2021-09-01 11:33:23 -0300

    fibonacci_recursive_v1

---
+++ segundo_projeto/fibonacci_recursive_v1.py
@@ -0,0 +1,13 @@
+#! /usr/bin/python
+
+# 0, 1, 1, 2, 3, 5, 8, 13, 21...
+def fibonacci(quantidade, sequencia=(0, 1)):
+    if len(sequencia) == quantidade:
+        return sequencia
+
+    return fibonacci(quantidade, sequencia + (sum(sequencia[-2:]),))
+
+
+if __name__ == '__main__':
+    for fib in fibonacci(20):
+        print(fib)
