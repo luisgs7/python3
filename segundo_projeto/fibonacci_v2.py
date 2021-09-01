366135c94efafc53ff48c934293bb9a909c51d8d
luisgs7 <luisgsilva270@gmail.com>
2021-09-01 11:32:19 -0300

    fibonacci_v2

---
+++ segundo_projeto/fibonacci_v2.py
@@ -0,0 +1,17 @@
+#! /usr/bin/python
+
+# 0, 1, 1, 2, 3, 5, 8, 13, 21...
+def fibonacci(limite):
+    penultimo = 0
+    ultimo = 1
+    print(f'{penultimo},{ultimo}', end=',')
+
+    while penultimo < limite:
+        proximo = penultimo + ultimo
+        print(proximo, end=',')
+        penultimo = ultimo
+        ultimo = proximo
+
+
+if __name__ == '__main__':
+    fibonacci(10000)
