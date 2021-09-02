40f107639ea0eeea78d6a8f48a704d4f6d9efaa7
luisgs7 <luisgsilva270@gmail.com>
2021-09-02 10:52:29 -0300

    comprehension_v1

---
+++ list_comprehension/comprehension_v1.py
@@ -0,0 +1,12 @@
+# [ expressão for item in list]
+
+dodros = [i * 2 for i in range(10)]
+print(dodros)
+
+# Versão "normal"
+dobros = []
+
+for i in range(10):
+    dobros.append(i * 2)
+
+print(dobros)
