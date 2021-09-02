547a80eb58e751568aa3808ea5047c7ab22807e6
luisgs7 <luisgsilva270@gmail.com>
2021-09-02 11:49:30 -0300

    gerador_html_v1

---
+++ funcoes/gerador_html_v1.py
@@ -0,0 +1,12 @@
+def tag_bloco(texto, classe='sucess'):
+    return f'<div class="{classe}">{texto}</div>'
+
+
+if __name__ == '__main__':
+    assert tag_bloco('Incluído com sucesso!') == \
+        '<div class="sucess">Incluído com sucesso!</div>'
+
+    assert tag_bloco('Impossível excluir!', 'error') == \
+        '<div class="error">Impossível excluir!</div>'
+
+    print(tag_bloco('bloco'))
