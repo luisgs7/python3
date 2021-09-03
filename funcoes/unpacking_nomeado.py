329668d24f9db8ae005cc7d004d190a87786921b
luisgs7 <luisgsilva270@gmail.com>
2021-09-03 11:24:15 -0300

    unpacking_nomeado

---
+++ funcoes/unpacking_nomeado.py
@@ -0,0 +1,11 @@
+def resultado_f1(primeiro, segundo, terceiro):
+    print(f'1) {primeiro}')
+    print(f'2) {segundo}')
+    print(f'3) {terceiro}')
+
+
+if __name__ == '__main__':
+    podium = {'segundo': 'L. Hamilton',
+              'primeiro': 'M. Verstappen',
+              'terceiro': 'S. Vettel'}
+    resultado_f1(**podium)
