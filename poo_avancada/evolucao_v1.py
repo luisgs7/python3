1225d5b8570cf83e5925d62984669faeca4311f5
luisgs7 <luisgsilva270@gmail.com>
2021-09-09 15:26:40 -0300

    evolucao_v1

---
+++ poo_avancada/evolucao_v1.py
@@ -0,0 +1,19 @@
+class Humano:
+    # atributo de classe
+    especie = 'Homo Sapiens'
+
+    def __init__(self, nome):
+        self.nome = nome
+
+    def das_carvernas(self):
+        self.especie = 'Homo Neanderthalensis'
+        return self
+
+
+if __name__ == '__main__':
+    jose = Humano('José')
+    grokn = Humano('Grokn').das_carvernas()
+
+    print(f'Humano.especie: {Humano.especie}')
+    print(f'jose.especie: {jose.especie}')
+    print(f'grokn.especie: {grokn.especie}')
