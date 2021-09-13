f7007484ac2fff7fbe828fab517471d72e474570
luisgs7 <luisgsilva270@gmail.com>
2021-09-13 10:36:36 -0300

    update config_database

--- banco_de_dados/config_dabase.sh
+++ banco_de_dados/config_dabase.sh
@@ -1,7 +1,7 @@
 # Script start MariaDB docker
 
 sudo docker run -e MARIADB_ROOT_PASSWORD=password -v </path/mysql>:/var/lib/mysql -d mariadb
-
+                   MARIADB_ALLOW_EMPTY_ROOT_PASSWORD 
 # Docker 
 
 sudo docker inspect <id>
