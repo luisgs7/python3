29e16d65b471e04dea2e45fd5bde7d7236cc57e9
luisgs7 <luisgsilva270@gmail.com>
2021-09-14 11:55:13 -0300

    consultarComJoin

---
+++ banco-relacional/consultarComJoin.sql
@@ -0,0 +1,25 @@
+SELECT 
+	e.nome AS Estado,
+    c.nome AS Cidade,
+    regiao as Regiao
+FROM estados e, cidades c
+WHERE e.id = c.estado_id;
+
+
+SELECT 
+	c.nome AS Cidade,
+    e.nome AS Estado,
+    regiao AS Regiao
+FROM estados AS e 
+INNER JOIN cidades c
+	on e.id = c.estado_id;
+    
+
+
+
+
+
+
+
+
+
