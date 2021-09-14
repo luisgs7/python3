1bcffa603f7df75427278abfc5234731a1adc1fa
luisgs7 <luisgsilva270@gmail.com>
2021-09-14 11:53:06 -0300

    inserirNovosEstados

---
+++ banco-relacional/inserirNovosEstados.sql
@@ -0,0 +1,7 @@
+INSERT INTO estados (id, nome, sigla, regiao, populacao)
+VALUES (1000, "Novo", "Nv", "Sul", 2.54);
+
+INSERT INTO estados (nome, sigla, regiao, populacao)
+VALUES ("Mais novo", "MN", "Norte", 2.51);
+
+SELECT * FROM estados;
