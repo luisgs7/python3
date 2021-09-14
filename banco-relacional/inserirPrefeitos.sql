91a24736efee2a39e85573a39429d2c03cc74424
luisgs7 <luisgsilva270@gmail.com>
2021-09-14 15:44:51 -0300

    inserirPrefeitos

---
+++ banco-relacional/inserirPrefeitos.sql
@@ -0,0 +1,11 @@
+
+INSERT INTO prefeitos
+	(nome, cidade_id)
+VALUES 
+    ('Rodrigo Neves', 7),
+    ('Raquel Lyra', 9),
+    ('Rodrigo Pinheiro', 10),
+    ('Rafael Greca', null),
+    ('Zenaldo Coutinho', null);
+    
+show tables;
