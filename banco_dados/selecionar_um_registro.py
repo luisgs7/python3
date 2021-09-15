ed69e40b5c2967f6f95dd0dd9fc782561abe5c7f
luisgs7 <luisgsilva270@gmail.com>
2021-09-15 15:45:51 -0300

    selecionar_um_registro

---
+++ banco_dados/selecionar_um_registro.py
@@ -0,0 +1,15 @@
+from os import curdir
+from db import nova_conexao
+
+sql = 'SELECT tel, nome FROM contatos LIMIT 3 OFFSET 10;'
+
+with nova_conexao() as conexao:
+    cursor = conexao.cursor()
+    cursor.execute(sql)
+
+    print(cursor.fetchone())
+    print(cursor.fetchone())
+    print(cursor.fetchone())
+    print(cursor.fetchone())
+    print(cursor.fetchone())
+    print(cursor.fetchone())
