b937b890d581e8a77c19ae7ca17f0901a0b4cb8a
luisgs7 <luisgsilva270@gmail.com>
2021-09-15 11:10:11 -0300

    excluir_tabela

---
+++ banco_dados/excluir_tabela.py
@@ -0,0 +1,10 @@
+from os import curdir
+from mysql.connector.errors import ProgrammingError
+from db import nova_conexao
+
+with nova_conexao() as conexao:
+    try:
+        cursor = conexao.cursor()
+        cursor.execute('DROP TABLE IF EXISTS emails')
+    except ProgrammingError as e:
+        print(f'Erro: {e.msg}')
