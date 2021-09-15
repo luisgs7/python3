7ef74ca8b4d8a37f0fb5af43b4150dbdcde35185
luisgs7 <luisgsilva270@gmail.com>
2021-09-15 15:46:53 -0300

    ordenar_contatos

---
+++ banco_dados/ordenar_contatos.py
@@ -0,0 +1,10 @@
+from codecs import register
+from db import nova_conexao
+
+sql = 'SELECT nome, id FROM contatos ORDER BY nome DESC'
+
+with nova_conexao() as conexao:
+    cursor = conexao.cursor()
+    cursor.execute(sql)
+
+    print('\n'.join(str(registro) for registro in cursor))
